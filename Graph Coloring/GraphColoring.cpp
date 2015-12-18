7cbb2ac3912c39f47753c5c887f01c421fb17668
zare3 <Home@MacBooks-iMac.local>
2015-12-18 19:20:27 +0200

    Update in file structure

--- Graph Coloring/GraphColoring.cpp
+++
@@ -1,5 +0,0 @@
-
-//#include "GraphColoring.h"
-
-
-
--- Graph Coloring/main.cpp
+++ Graph Coloring/main.cpp
@@ -1,14 +1,31 @@
 
-/* Graph Coloring Using Genetic Algorithms*/
-/* Copyrights @zare3 2015*/
-/*For more info, don't hesitate to contact me at zare3@aucegypt.edu*/
+//
+// Disclamer:
+// ----------
+//
+// This code will work only if you selected window, graphics and audio.
+//
+// Note that the "Run Script" build phase will copy the required frameworks
+// or dylibs to your application bundle so you can execute it on any OS X
+// computer.
+//
+// Your resource files (images, sounds, fonts, ...) are also copied to your
+// application bundle. To get the path to these resource, use the helper
+// method resourcePath() from ResourcePath.hpp
+//
 
 
 
+// Here is a small helper for you ! Have a look.
 #include "GraphColoring.h"
 
+/*
 
-
+ string inputFileNames [16] = {"anna.col","david.col","fpsol2.i.1.col","games120.col","homer.col","huck.col","jean.col","miles250.col",
+ "miles1000.col", "myciel3.col", "myciel4.col", "myciel5.col", "queen5_5.col", "queen6_6.col", "queen7_7.col",
+ "queen8_8.col"};
+ */
+ 
 
 
 int main(int, char const**)
@@ -16,100 +33,58 @@ int main(int, char const**)
     
     srand(time(NULL));
     
-    /* IF YOU WANT US TO GENERATE A RANDOM GRAPH FOR YOU*/
     
-    N = 10; // number of nodes in the graph
-    
-    /* you can do this by not sending an argument to colorGraph() -- see below in the code */
-    
-    
-    
-    
-    /*------------------------------------------------------------------------------------------------------------------------*/
-    /*------------------------------------------------------------------------------------------------------------------------*/
-    /*------------------------------------------------------------------------------------------------------------------------*/
-    
-    
-    /* if you send an argument to colorGraph(idx), it will use idx to generate a graph according to this dictionary of graphs
-     
-     "anna.col"             0
-     "david.col"            1
-     "fpsol2.i.1.col"       2
-     "games120.col"         3
-     "homer.col"            4
-     "huck.col"             5
-     "jean.col"             6
-     "miles250.col"         7
-     "miles1000.col"        8
-     "myciel3.col"          9
-     "myciel4.col"          10
-     "myciel5.col"          11
-     
-     The graphs could be found in /input data OR you can find them here: http://mat.gsia.cmu.edu/COLOR/instances.html
-     
-     ---- if you want to add more graphs to the dictionary, you can do so by this statement:
-     ---- inputFileNames[i] = "newGraph"; -- i should start from 12 -- and maximumly: 2999
-     ---- such that newGraph.txt should be in the /input data folder, and should abide by the same descripting syntax in the anna.col, david.col, .... ---
-     
-     */
-    
-    
-
-    
-    /*------------------------------------------------------------------------------------------------------------------------*/
-    /*------------------------------------------------------------------------------------------------------------------------*/
-    /*------------------------------------------------------------------------------------------------------------------------*/
-    
-    
-    
-    /*relative path to search for the /input data and to search for /results*/
-    /* if you're using an OS that supports relative paths, leave this empty*/
-    
-    
-    //relativePath = ""; // windows or linux
-    
-    
-    relativePath = "/Users/Home/AUC/Fall 15/Neural/Project/Graph Coloring/"; // osx
-    
-    
-    
-    /*------------------------------------------------------------------------------------------------------------------------*/
-    /*------------------------------------------------------------------------------------------------------------------------*/
-    /*------------------------------------------------------------------------------------------------------------------------*/
-    
-    
-    
-    /* Genetic Algorithm Variables */
-    popSize = 200; // population size
-    maxFitness = -0.1; // stop when fitness reaches maxFitness
-    mutationRate = 100; //for each 2 new children, i select one randomly to mutate, but this process happens with a mutationRate possibility (0 - 100)
-    maxIterations = 5000; // when you reach maxIterations, end training
-    patienceTime = 70; // when generations saturate for patienceTime generations, the helper function gets called
-    maxUselessShuffle = 2 ; // maximum number of times to call the helper function
-    // the helper function is a function designed to kill saturation and increase maximum fitness
-    
-    
-    
-    
-    
-    /*------------------------------------------------------------------------------------------------------------------------*/
-    /*------------------------------------------------------------------------------------------------------------------------*/
-    /*------------------------------------------------------------------------------------------------------------------------*/
-    
-    
+    N = 10;
+    maxNodesToBeRand = 16;
+    popSize = 200;
+    maxFitness = -0.1;
+    mutationRate = 100;
+    maxIterations = 5000;
+    maxEdgeCnt = 5*N;
+    patienceTime = 70;
+    maxUselessShuffle = 2 ;
     
     for (int i=0; i<12; i++){
-        
-        try{
-            colorGraph(i);
-            
-        }
-        catch (string err)
-        {
-            cout << err << endl;
-        }
+    
+        buildGraph(i);
     }
+    //loadUIVariables ();
     
     cout << " END " << endl;
     return 0;
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(1200, 600), "Colored Graph");
+    double scale = min ( window.getSize().x/(float)img.getSize().x, window.getSize().y/(float)img.getSize().y );
+    scale/=1.1;
+    imgSprite.setScale( scale, scale);
+    
+    // Start the game loop
+    while (window.isOpen() && !DIMACS)
+    {
+        
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            
+            // Escape pressed: exit
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+                window.close();
+            }
+        }
+        
+        // update the UI
+        refreshUI(&window);
+        //---------------------
+            
+    }
+        
+        
+    
+    
+    return EXIT_SUCCESS;
 }
